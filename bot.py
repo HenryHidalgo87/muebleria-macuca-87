a7402174e5691b0741880748677d125217edc01a
Henry Hidalgo <henryhidalgo870@gmail.com>
2021-07-29 16:53:24 -0400

    Not Now

--- bot.py
+++ bot.py
@@ -302,7 +302,7 @@ def end(update: Update, context: CallbackContext) -> int:
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1630057239:AAGno5APPSjhkE8_S4AV56EpUAH51CTCQVY")
+    updater = Updater("")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
